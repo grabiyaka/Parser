2965ccc8f2c0849b03f02c9a2c415a40ac590efb
rabiyaka <illya.gerasimets@gmail.com>
2023-07-07 16:36:45 +0300

    update

--- functions.php
+++ functions.php
@@ -1,5 +1,19 @@
 <?php
 
+set_time_limit(2000);
+
+function updateXML()
+{
+    require __DIR__ . "/phpQuery-onefile.php";
+    require __DIR__ . "/functions.php";
+    require __DIR__ . "/parsers/formdekor.php";
+    require __DIR__ . "/parsers/techno-odis.php";
+    $formdekorData = getFormdekorData();
+    file_put_contents('xml/formdekor.xml', $formdekorData);
+    $technoOdisData = getTechnoOdisData();
+    file_put_contents('xml/techno-odis.xml', $technoOdisData);
+}
+
 function dd($value)
 {
     echo '<pre>';
