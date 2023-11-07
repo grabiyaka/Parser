7e522f4163fbd4335df796ba808ab706103fc20a
rabiyaka <illya.gerasimets@gmail.com>
2023-11-07 11:53:52 +0200

    index.php as on the server

--- index.php
+++ index.php
@@ -1,39 +1,36 @@
 <?php
-require __DIR__ . '/functions.php';
-require __DIR__ . '/phpQuery-onefile.php';
-require __DIR__ . '/parsers/techno-odis.php';
-require __DIR__ . '/parsers/formdekor.php';
+// Убедитесь, что загрузка файлов разрешена в настройках PHP
+ini_set('file_uploads', 'On');
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-$formdekorData = getFormdekorData();
-file_put_contents('xml/formdekor.xml', $formdekorData);
-$technoOdisData = getTechnoOdisData();
-file_put_contents('xml/techno-odis.xml', $technoOdisData);
 
-$url = 'https://parser-for-grabiyaka.000webhostapp.com';
-
-$file1Path = 'xml/formdekor.xml';
-$file2Path = 'xml/techno-odis.xml';
-
-$data = array(
-    'formdekor' => new CURLFile($file1Path),
-    'techno-odis' => new CURLFile($file2Path)
-);
-
-$curl = curl_init();
-
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-$result = curl_exec($curl);
-
-curl_close($curl);
-
-if ($result !== false) {
-    echo $result;
+// Обработка загруженных файлов
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+   
+   if(isset($_FILES['parser_data'])){
+        $file1 = $_FILES['parser_data'];
+        // Проверяем наличие ошибок при загрузке файлов
+        if ($file1['error'] === UPLOAD_ERR_OK) {
+            $targetDir = 'xml/'; 
+    
+            $fileName1 = basename($file1['name']);
+            $targetFilePath1 = $targetDir . $fileName1;
+            if (move_uploaded_file($file1['tmp_name'], $targetFilePath1)) {
+                echo 'XML-файл успешно загружен и сохранен.<br>';
+            } else {
+                echo 'Ошибка при сохранении первого XML-файла.<br>';
+            }
+            
+        }
+    } else{
+        echo "no file parser_data(";
+    }
+   
+    
 } else {
-    echo 'Ошибка при выполнении запроса.';
+    echo 'Ошибка загрузки XML-файлов.<br>';
 }
 
 ?>
